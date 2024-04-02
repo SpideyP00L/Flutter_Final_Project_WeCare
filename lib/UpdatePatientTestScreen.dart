2a8fe5bd86395c313052e236677635b80c0aef95
mudrap27 <mudrap27@gmail.com>
2024-04-01 21:40:52 -0400

    verified the screens design are changing based on the screen size

--- lib/UpdatePatientTestScreen.dart
+++ lib/UpdatePatientTestScreen.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
+
 class UpdatePatientTestScreen extends StatefulWidget {
   final String patientId;
 
